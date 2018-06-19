853694c344264dadb6a2556375bde2a6b5371da4
MaheshwaranG <gmaheshwaranit@gmail.com>
2018-06-20 00:03:47 +0530

    First Commit

---
+++ maheshgo-programming/maheshgo-java/hackerrank/PrimeTest1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+
+public class PrimeTest1 {
+
+	public static void main(String args[]){
+		Scanner in = new Scanner(System.in);
+//		int n = in.nextInt();
+		for(int i=1;i<50;i++)
+			System.out.println("i is "+ i +" " +(i&1));
+				
+		long l = 739420647;
+		
+	}
+}
